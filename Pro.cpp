5863b86ee9f11ddec9074500397c2df742c7a5aa
Jisusboy <moran20101@hotmail.com>
2015-05-19 22:29:51 -0500

    Update and rename .gitignore to Pro.cpp

---
+++ Pro.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+# include <windows .h>
+
+using namespace std;
+
+char NomUser
+char PassWord
+
+void login(){
+
+cout<<"Ingrese un Nombre de Usuario: ";cin>>NomUser;cout<<endl
+cout<<"Ingrese una Contraseña";cin>>PassWord;
+
+}
+
