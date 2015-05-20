24d53b90e6480d6da77c265812bc4b2967339b8d
Jisusboy <moran20101@hotmail.com>
2015-05-20 13:00:34 -0500

    Update Pro.cpp

--- Pro.cpp
+++ Pro.cpp
@@ -14,3 +14,25 @@ cout<<"Ingrese una Contraseña";cin>>PassWord;
 }
 
 inline BorInf () {system ("cls")}
+
+int main(){
+   BorInf ();
+   cout<<"Selecciones una opcion"<<endl;
+   cout<<"1. Crear Cuenta"<<endl;
+   cout<<"2. Iniciar Sesion"<<endl;
+   cout<<"3. Apagar"<<endl;
+   cout<<endl;
+   cout<<endl;
+   cout<<"opcines 1,2,3":cin>>sch;
+   return sch;
+}
+
+int main() 
+{
+    GestData();
+    
+    InSes:
+    
+    InSes();
+    
+}
