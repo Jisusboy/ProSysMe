a20c0ad0ece5cd0f46bd69fa264a3c5c408b4113
Jisusboy <moran20101@hotmail.com>
2015-05-23 17:42:45 -0500

    Update and rename Pro.cpp to CoInSis.cpp

---
+++ CoInSis.cpp
@@ -0,0 +1,46 @@
+# include <iostream>
+# include <windows .h>
+
+using namespace std;
+
+char In
+char Cc
+
+Protected: char NomUser
+           char PassWord
+
+void In(){
+
+cout<<"Seleccione una opcion: "<<endl;
+cout<<"Crear Cuenta"<<cin>>char,Cc;endl;
+cout<<"Iniciar Sesion"<<cin>>char,In;endl;
+cout<<"Salir"<<endl
+cout<<endl
+
+retun (sch);
+}
+
+inline BorInf () {system ("cls")}
+
+int main(){
+   BorInf ();
+   
+   void 
+   
+   swich ()
+   {
+      case 1 
+       
+      break;
+      case 2 
+      
+      break;
+      case 3 
+      
+      break;
+      case 4 
+      
+      break;
+      return (0);
+   }
+}
